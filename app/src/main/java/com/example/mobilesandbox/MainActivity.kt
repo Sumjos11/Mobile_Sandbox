ce0fafc45a7b1e8a76df87e7f9cf74ee32458d95
Sumjos11 <josephsummers64@gmail.com>
2020-04-26 04:17:24 -0400

    Added some code!

--- app/src/main/java/com/example/mobilesandbox/MainActivity.kt
+++ app/src/main/java/com/example/mobilesandbox/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.mobilesandbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(context: this, "Hello from Android!", Toast.LENGTH_LONG).show()
+
     }
 }
